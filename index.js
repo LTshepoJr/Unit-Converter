bf3f3a2b6de62e541be8a2591b68a1308c32eefd
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-07-03 04:36:30 +0200

    removed innerHTML and used createElement

--- index.js
+++ index.js
@@ -1,81 +1,52 @@
 const meterToFeet = 3.281;
-const literToGallon = 0.264;
-const kilogramToPound = 2.204;
+const literToGallon = 0.264172;
+const kilogramToPound = 2.205;
 const numberInput = document.getElementById("number-input");
 const length = document.getElementById("length");
 const button = document.getElementById("btn");
 const volume = document.getElementById("volume");
 const mass = document.getElementById("mass");
 
-// const displayConversions = (number, feet, meter, pElement) => {
-//   const p = document.createElement("p");
-//   const span1 = document.createElement("span");
-//   const span2 = document.createElement("span");
+const displayConversions = (number, input1, input2, pElement, text1, text2) => {
+  const p = document.createElement("p");
+  const span1 = document.createElement("span");
+  const span2 = document.createElement("span");
 
-//   p.appendChild(document.createTextNode(`${number} meters = `));
-//   span1.textContent = `${feet} feet`;
-//   p.appendChild(span1);
-//   p.appendChild(document.createTextNode(` | ${number} feet = `));
-//   span2.textContent = `${meter} meters`;
-//   p.appendChild(span2);
+  p.appendChild(document.createTextNode(`${number} ${text2} = `));
+  span1.textContent = `${input1} ${text1}`;
+  p.appendChild(span1);
+  p.appendChild(document.createTextNode(` | ${number} ${text1} = `));
+  span2.textContent = `${input2} ${text2}`;
+  p.appendChild(span2);
 
-//   pElement.appendChild(p);
-// };
+  pElement.appendChild(p);
+};
 
 function lengthConvert() {
   const number = numberInput.value;
   const feet = (number * meterToFeet).toFixed(3);
   const meter = (number / meterToFeet).toFixed(3);
-  //   length.insertAdjacentHTML(
-  //     "beforeend",
-  //     `<p>
-  //       ${number} meters = <span>${feet} feet</span> | ${number} feet =
-  //       <span>${meter} meters</span>
-  //     </p>`
-  //   );
-  //   displayConversions(number, feet, meter, length);
-  length.innerHTML = `<p>
-    ${number} meters = <span>${feet} feet</span> | ${number} feet =
-    <span>${meter} meters</span>
-  </p>`;
+  displayConversions(number, feet, meter, length, "feet", "meters");
 }
 
 function volumeConvert() {
   const number = numberInput.value;
   const gallon = (number * literToGallon).toFixed(3);
   const liter = (number / literToGallon).toFixed(3);
-  //   volume.insertAdjacentHTML(
-  //     "beforeend",
-  //     `<p>
-  //       ${number} liters = <span>${gallon} gallons</span> | ${number} gallons =
-  //       <span>${liter} liters</span>
-  //     </p>`
-  //   );
-  volume.innerHTML = `<p>
-    ${number} liters = <span>${gallon} gallons</span> | ${number} gallons =
-    <span>${liter} liters</span>
-  </p>`;
+  displayConversions(number, gallon, liter, volume, "gallons", "liters");
 }
 
 function massConvert() {
   const number = numberInput.value;
   const pound = (number * kilogramToPound).toFixed(3);
   const kilogram = (number / kilogramToPound).toFixed(3);
-  //   mass.insertAdjacentHTML(
-  //     "beforeend",
-  //     `<p>
-  //       ${number} kilos = <span>${pound} pounds</span> | ${number} pounds =
-  //       <span>${kilogram} kilos</span>
-  //     </p>`
-  //   );
-  mass.innerHTML = `<p>
-    ${number} kilos = <span>${pound} pounds</span> | ${number} pounds =
-    <span>${kilogram} kilos</span>
-  </p>`;
+  displayConversions(number, pound, kilogram, mass, "pounds", "kilograms");
 }
 
-button.addEventListener("click", () => {
+button.addEventListener("click", function handleClick() {
   lengthConvert();
   massConvert();
   volumeConvert();
+  button.removeEventListener("click", handleClick);
+  button.disabled = true;
 });
